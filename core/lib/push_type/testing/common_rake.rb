fb27a5fa5ead78db26f41a5ebf69dcec26b4afa3
Aaron Russell <aaron@gc4.co.uk>
2014-12-10 17:57:54 +0000

    Fix test failing due to nilclass on node#type.

--- core/lib/push_type/testing/common_rake.rb
+++ core/lib/push_type/testing/common_rake.rb
@@ -7,11 +7,11 @@ namespace :common do
   task :test_app, :lib_name, :base_path, :dummy_path do |t, args|
     args.with_defaults lib_name: 'push_type_core', base_path: './', dummy_path: 'test/dummy'
 
-    PushType::DummyGenerator.start ["--lib_name=#{ args[:lib_name] }", "--path=#{ args[:dummy_path] }", "--quiet"]
+    PushType::DummyGenerator.start ["--lib_name=#{ args[:lib_name] }", "--path=#{ args[:dummy_path] }", '--quiet']
 
     Dir.chdir File.join(args[:base_path], args[:dummy_path]) do
-      PushType::InstallGenerator.start ["--migrate=false", "--quiet"]
-      PushType::NodeGenerator.start ["page", "--quiet"]
+      PushType::InstallGenerator.start ['--migrate=false', '--quiet']
+      PushType::NodeGenerator.start ['page', '--quiet']
       system 'bundle exec rake db:drop db:create db:migrate'
     end
   end
