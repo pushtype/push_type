42c2adae3a4768511922e45967ee0295bd163057
Aaron Russell <aaron@gc4.co.uk>
2015-02-02 19:05:21 +0000

    Allow custom sorting of node children through nestable concern.

--- core/app/models/concerns/push_type/nestable.rb
+++ core/app/models/concerns/push_type/nestable.rb
@@ -6,7 +6,22 @@ module PushType
       self.class.child_node_types
     end
 
+    def custom_child_order?
+      self.class.child_node_order.present?
+    end
+
+    def sortable?
+      !custom_child_order?
+    end
+
+    def children
+      return super() unless custom_child_order?
+      super.reorder(self.class.child_node_order)
+    end
+
     module ClassMethods
+
+      attr_reader :child_node_order
 
       def child_node_types
         types = @child_node_types || PushType.config.root_node_types
@@ -14,6 +29,15 @@ module PushType
       end
 
       def has_child_nodes(*args)
+        if args.last.is_a? Hash
+          opts = args.pop
+          @child_node_order = case opts[:order]
+            when :blog then ['published_at DESC', 'created_at DESC']
+            else opts[:order]
+          end
+        else
+          @child_node_order = nil
+        end
         @child_node_types = args
       end
 
--- core/test/models/concerns/push_type/nestable_test.rb
+++ core/test/models/concerns/push_type/nestable_test.rb
@@ -11,9 +11,13 @@ module PushType
 
     describe '.has_child_nodes' do
       describe 'defaults' do
-        before { TestPage.instance_variable_set '@child_node_types', nil }
+        before do
+          TestPage.instance_variable_set '@child_node_types', nil
+          TestPage.instance_variable_set '@child_node_order', nil
+        end
         it { TestPage.child_node_types.must_equal roots }
         it { page.child_node_types.must_equal roots }
+        it { page.must_be :sortable? }
       end
 
       describe 'when none' do
@@ -39,6 +43,31 @@ module PushType
         it { TestPage.child_node_types.must_equal [] }
         it { page.child_node_types.must_equal [] }
       end
+
+      describe 'without options' do
+        before { TestPage.has_child_nodes :page, :test_page }
+        it { TestPage.child_node_types.must_equal ['page', 'test_page'] }
+        it { page.custom_child_order?.must_equal false }
+        it { page.must_be :sortable? }
+        it { page.children.order_values.must_include 'sort_order' }
+      end
+
+      describe 'with options' do
+        before { TestPage.has_child_nodes :page, :test_page, order: 'name ASC' }
+        it { TestPage.child_node_types.must_equal ['page', 'test_page'] }
+        it { page.custom_child_order?.must_equal true }
+        it { page.wont_be :sortable? }
+        it { page.children.order_values.must_include 'name ASC' }
+      end
+
+      describe 'with template' do
+        before { TestPage.has_child_nodes :page, :test_page, order: :blog }
+        it { TestPage.child_node_types.must_equal ['page', 'test_page'] }
+        it { page.custom_child_order?.must_equal true }
+        it { page.wont_be :sortable? }
+        it { page.children.order_values.must_equal ['published_at DESC', 'created_at DESC'] }
+      end
+
     end
 
   end
