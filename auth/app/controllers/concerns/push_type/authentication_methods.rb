042b464b7ef7d4b9e223e3ddd4ea68dcb3b74b38
Aaron Russell <aaron@gc4.co.uk>
2015-02-08 20:40:08 +0000

    Override authenticate_user! so redirect goes to correct path.

--- auth/app/controllers/concerns/push_type/authentication_methods.rb
+++ auth/app/controllers/concerns/push_type/authentication_methods.rb
@@ -6,5 +6,15 @@ module PushType
       before_filter :authenticate_user!
     end
 
+    protected
+
+    def authenticate_user!
+      if user_signed_in?
+        super
+      else
+        redirect_to push_type.new_user_session_path, alert: t('devise.failure.unauthenticated')
+      end
+    end
+
   end
 end
