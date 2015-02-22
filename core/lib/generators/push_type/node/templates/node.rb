503c374c45f7f447deea36681da93d0470cea065
Aaron Russell <aaron@gc4.co.uk>
2015-02-22 14:44:03 +0000

    Define fields with node generator.

--- core/lib/generators/push_type/node/templates/node.rb
+++ core/lib/generators/push_type/node/templates/node.rb
@@ -4,7 +4,9 @@ class <%= class_name %> < PushType::Node
   # has_child_nodes :all
 
   # Set custom fields
-  # field :description
   # field :body, :text, validates: { presence: true }
+<% attributes.each do |att| -%>
+  <%= attribute_as_field(att) %>
+<% end -%>  
 
 end
--- core/test/lib/generators/push_type/node_generator_test.rb
+++ core/test/lib/generators/push_type/node_generator_test.rb
@@ -8,10 +8,14 @@ module PushType
 
     before :all do
       prepare_destination
-      run_generator ['home_page']
+      run_generator ['home_page', 'foo', 'bar:text']
     end
 
     it { assert_file 'app/models/home_page.rb', %r{class HomePage < PushType::Node} }
+    it { assert_file 'app/models/home_page.rb', %r{field :foo, :string} }
+    it { assert_file 'app/models/home_page.rb', %r{field :bar, :text} }
     it { assert_file 'app/views/nodes/home_page.html.erb', %r{<h1><%= @node.title %></h1>} }
+    it { assert_file 'app/views/nodes/home_page.html.erb', %r{<div>Foo:</div>} }
+    it { assert_file 'app/views/nodes/home_page.html.erb', %r{<div><%= @node.foo %></div>} }
   end
 end
