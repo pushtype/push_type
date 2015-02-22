22369b7d5b1f73a8037eb9b1f6976cbf8787d867
Aaron Russell <aaron@gc4.co.uk>
2015-02-22 15:14:03 +0000

    Custom field generator.

---
+++ core/test/lib/generators/push_type/field_generator_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+require "generators/push_type/field/field_generator"
+
+module PushType
+  describe FieldGenerator do
+    tests FieldGenerator
+    destination Rails.root.join('tmp/generators')
+
+    before :all do
+      prepare_destination
+      run_generator ['rich_text']
+    end
+
+    it { assert_file 'app/fields/rich_text_field.rb', %r{class RichTextField < PushType::FieldType} }
+  end
+end
