bc9a5824f9b1052228a431e8cb364cab2e296723
Aaron Russell <aaron@gc4.co.uk>
2015-03-09 15:36:07 +0000

    Added presenter generator.

---
+++ core/lib/generators/push_type/presenter/templates/presenter.rb
@@ -0,0 +1,12 @@
+class <%= class_name %>Presenter < PushType::Presenter
+
+  # This class delegates to the model, <%= class_name %>
+  # Override any methods to add custom presentation logic
+  # View helpers are accessible from the `h` method
+
+  # Example:
+  # def title
+  #   h.content_tag :strong, super()
+  # end
+
+end
---
+++ core/test/lib/generators/push_type/presenter_generator_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+require "generators/push_type/presenter/presenter_generator"
+
+module PushType
+  describe PresenterGenerator do
+    tests PresenterGenerator
+    destination Rails.root.join('tmp/generators')
+
+    before :all do
+      prepare_destination
+      run_generator ['page']
+    end
+
+    it { assert_file 'app/presenters/page_presenter.rb', %r{class PagePresenter < PushType::Presenter} }
+  end
+end
