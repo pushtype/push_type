1bf240b2ad82eb11a7825f1cb35af40fc0be8b75
Aaron Russell <aaron@gc4.co.uk>
2015-06-01 18:22:22 +0100

    Fix empty tag_list array issue.

--- core/app/fields/push_type/tag_list_field.rb
+++ core/app/fields/push_type/tag_list_field.rb
@@ -11,10 +11,6 @@ module PushType
       super.reject(&:blank?) if val.present?
     end
 
-    def from_json(val)
-      super.reject(&:blank?) if val.present?
-    end
-
     initialized_on_node do |object, field|
       object.define_singleton_method "all_#{ field.name }".to_sym do |*args|
         TagListQuery.new(field.name, object.name.underscore.to_sym).all *args
