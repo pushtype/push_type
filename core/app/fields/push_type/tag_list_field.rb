63c0db4f682ce15e3f62900d7df3eed532659ef8
Aaron Russell <aaron@gc4.co.uk>
2015-07-19 20:48:10 +0100

    Removed TagListQuery class and replaced with TagListField dynamic methods.

--- core/app/fields/push_type/tag_list_field.rb
+++ core/app/fields/push_type/tag_list_field.rb
@@ -1,5 +1,3 @@
-require 'push_type/tag_list_query'
-
 module PushType
   class TagListField < PushType::FieldType
 
@@ -12,8 +10,66 @@ module PushType
     end
 
     initialized_on_node do |object, field|
-      object.define_singleton_method "all_#{ field.name }".to_sym do |*args|
-        TagListQuery.new(field.name, object.name.underscore.to_sym).all *args
+
+      object.class_eval do
+
+        # Dynamically define standard scope .with_all_`field_name`
+        # Returns ActiveRecord::Relation
+        #
+        scope "with_all_#{ field.name }".to_sym, ->(*args) {
+          raise ArgumentError, 'wrong number of arguments' unless args.present?
+          tags = args[0].is_a?(Array) ? args[0] : args
+
+          where(["field_store->'tags' ?& ARRAY[:tags]", { tags: tags }])
+        }
+
+
+        # Dynamically define class method .with_any_`field_name`
+        # Returns Array
+        #
+        define_singleton_method "with_any_#{ field.name }".to_sym do |*args, &block|
+          raise ArgumentError, 'wrong number of arguments' unless args.present?
+          tags = args[0].is_a?(Array) ? args[0] : args
+
+          composed_scope = (
+            block.respond_to?(:call) ? block.call : all
+          ).where(["field_store->'#{ field.name }' ?| ARRAY[:tags]", { tags: tags }])
+
+          t   = Arel::Table.new('t',  ActiveRecord::Base)
+          ct  = Arel::Table.new('ct', ActiveRecord::Base)
+
+          arr_sql = Arel.sql "ARRAY[#{ tags.map { |t| Arel::Nodes::Quoted.new(t).to_sql }.join(', ') }]"
+          any_tags_func = Arel::Nodes::NamedFunction.new('ANY', [arr_sql])
+
+          lateral = ct
+            .project(Arel.sql('e').count(true).as('ct'))
+            .from(Arel.sql "jsonb_array_elements_text(t.field_store->'#{ field.name }') e")
+            .where(Arel::Nodes::Equality.new Arel.sql('e'), any_tags_func)
+
+          query = t
+            .project(t[Arel.star])
+            .from(composed_scope.as('t'))
+            .join(Arel.sql ", LATERAL (#{ lateral.to_sql }) ct")
+            .order(ct[:ct].desc)
+
+          find_by_sql query.to_sql
+        end
+
+
+        # Dynamically define class method .all_`field_name`
+        # Returns Array
+        #
+        define_singleton_method "all_#{ field.name }".to_sym do |*args, &block|
+          composed_scope = block.respond_to?(:call) ? block.call : all
+          composed_scope.projections = []
+
+          query = composed_scope
+            .project(Arel.sql "jsonb_array_elements_text(field_store->'#{ field.name }') t")
+            .distinct
+            .order(Arel.sql 't')
+          connection.select_all(query.to_sql).rows.flatten
+        end
+
       end
     end
 
--- core/test/fields/push_type/tag_list_field_test.rb
+++ core/test/fields/push_type/tag_list_field_test.rb
@@ -20,13 +20,38 @@ module PushType
       before do
         TestPage.instance_variable_set '@fields', ActiveSupport::OrderedHash.new
         TestPage.field :tags, :tag_list
-        TestPage.create FactoryGirl.attributes_for(:node, tags: ['foo', 'bar'])
+        TestPage.create FactoryGirl.attributes_for(:node, tags: ['foo', 'bar', 'foo'])
         TestPage.create FactoryGirl.attributes_for(:node, tags: ['baz', 'bar'])
-        TestPage.create FactoryGirl.attributes_for(:node, tags: ['qux'])
+        TestPage.create FactoryGirl.attributes_for(:published_node, tags: ['qux'])
+        @first = TestPage.create FactoryGirl.attributes_for(:node, tags: ['foo', 'bar', 'qux'])
+        TestPage.create FactoryGirl.attributes_for(:published_node)
       end
       after { TestPage.instance_variable_set '@fields', ActiveSupport::OrderedHash.new }
-      it { TestPage.must_respond_to :all_tags }
-      it { TestPage.all_tags.must_equal ['bar', 'baz', 'foo', 'qux'] }
+
+      describe '#with_all_tags' do
+        let(:tags) { ['foo', 'foo', 'bar', 'qux'] }
+        it { TestPage.must_respond_to :with_all_tags }
+        it { TestPage.with_all_tags(tags).must_be_instance_of TestPage::ActiveRecord_Relation }
+        it { TestPage.with_all_tags(tags).count.must_equal 1 }
+        it { TestPage.published.with_all_tags(tags).count.must_equal 0 }
+        it { TestPage.published.with_all_tags('qux').count.must_equal 1 }
+      end
+
+      describe '#with_any_tags' do
+        let(:tags) { ['foo', 'foo', 'bar', 'qux'] }
+        it { TestPage.must_respond_to :with_any_tags }
+        it { TestPage.with_any_tags(tags).must_be_instance_of Array }
+        it { TestPage.with_any_tags(tags).count.must_equal 4 }
+        it { TestPage.with_any_tags(tags).first.must_equal @first }
+        it { TestPage.published.with_any_tags(tags).count.must_equal 1 }
+      end
+
+      describe '#all_tags' do
+        it { TestPage.must_respond_to :all_tags }
+        it { TestPage.all_tags.must_equal ['bar', 'baz', 'foo', 'qux'] }
+        it { TestPage.published.all_tags.must_equal ['qux'] }
+        it { TestPage.all_tags { TestPage.published }.must_equal ['qux'] }
+      end
     end
   end
 
