acf88cc5f92deaabf39e66e1e48268ccff96c024
Asher Van Brunt <asher@okbreathe.com>
2018-05-18 16:26:40 -0700

    Fix conflicts with apps using multiple engines

--- admin/lib/push_type/kaminari_patch.rb
+++ admin/lib/push_type/kaminari_patch.rb
@@ -2,12 +2,14 @@ module Kaminari
   module Helpers
     class Tag
 
+      alias :base_page_url_for :page_url_for
+
       def page_url_for(page)
         arguments = @params.merge(@param_name => (page <= 1 ? nil : page), only_path: true).symbolize_keys
         begin
           PushType::Admin::Engine.routes.url_helpers.url_for arguments
         rescue
-          @template.main_app.url_for arguments
+          base_page_url_for(page)
         end
       end
 
